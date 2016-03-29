03204047d475de76fab508d43754adfce448c1f7
Axel Martin <axel.martin@eisti.fr>
2016-03-29 13:51:54 +0000

    featureJobQueue: refactoring the debug message management

--- src/job.h
+++ src/job.h
@@ -1,8 +1,10 @@
-#ifndef _H_JOB_QUEUE
-#define _H_JOB_QUEUE
+#ifndef _H_JOB
+#define _H_JOB
 
 
-#include <iostream>  // FIXME: for debug purpose only
+#include "scheduler.h"
+
+#include <iostream>
 #include <ctime>
 #include <queue>
 
@@ -33,12 +35,6 @@ class Job {
 
 };
 
-
-struct JobComparator {
-    bool operator()(const Job& left, const Job& right) {
-        return left < right;
-    }
-};
 
 
 typedef std::priority_queue<Job, std::vector<Job>> JobQueue;
---
+++ src/main.h
@@ -0,0 +1,13 @@
+#ifndef _H_MAIN
+#define _H_MAIN
+
+
+#include "scheduler.h"
+
+#include <iostream>
+#include <vector>
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+
+
+#endif
