5aca93a78fbef4460a4daa305335d6530328eeca
Axel Martin <axel.martin@eisti.fr>
2016-03-29 13:51:54 +0000

    featureJobQueue: refactoring the debug message management

--- src/job.cpp
+++ src/job.cpp
@@ -8,14 +8,14 @@ int aging_calculation(std::time_t t1, std::time_t t2) {
 
 Job::Job(std::string command_line, int burst_time): command_line(command_line), burst_time(burst_time) {
     timestamp = std::time(nullptr);
-    cout << "Job initialization (short sequence)" << endl;  // FIXME: for debug purpose only
+    debug("Job initialization (short sequence)" << endl);
 }
 
 
 Job::Job(std::string command_line, int burst_time, int user_priority, int cpu_load):
     command_line(command_line), burst_time(burst_time), user_priority(user_priority), cpu_load(cpu_load) {
     timestamp = std::time(nullptr);
-    cout << "Job initialization (long sequence)" << endl;  // FIXME: for debug purpose only
+    debug("Job initialization (long sequence)" << endl);
 }
 
 
