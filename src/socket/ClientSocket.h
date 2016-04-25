99be2e6205fc489058d88b0837d90f77536026d5
Axel Martin <axel.martin@eisti.fr>
2016-04-25 16:42:30 +0000

    featurePortocolLayer: Sockets class syntax refactoring

--- src/socket/ClientSocket.h
+++ src/socket/ClientSocket.h
@@ -5,18 +5,14 @@
 
 #include "Socket.h"
 
-
 class ClientSocket : private Socket
 {
- public:
+   public:
+    ClientSocket(std::string host, int port);
+    virtual ~ClientSocket(){};
 
-  ClientSocket ( std::string host, int port );
-  virtual ~ClientSocket(){};
-
-  const ClientSocket& operator << ( const std::string& ) const;
-  const ClientSocket& operator >> ( std::string& ) const;
-
+    const ClientSocket& operator<<(const std::string&) const;
+    const ClientSocket& operator>>(std::string&) const;
 };
-
 
 #endif
--- src/socket/Socket.h
+++ src/socket/Socket.h
@@ -3,7 +3,6 @@
 #ifndef Socket_class
 #define Socket_class
 
-
 #include <iostream>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -15,44 +14,44 @@
 #include <arpa/inet.h>
 #include <memory>
 
-
-const int MAXHOSTNAME = 200;
+const int MAXHOSTNAME    = 200;
 const int MAXCONNECTIONS = 5;
-const int MAXRECV = 500;
+const int MAXRECV        = 500;
 
 class Socket
 {
- public:
-  Socket();
-  virtual ~Socket();
+   public:
+    Socket();
+    virtual ~Socket();
 
-  // Server initialization
-  bool create();
-  bool bind ( const int port );
-  bool listen() const;
-  bool accept ( Socket& ) const;
+    // Server initialization
+    bool create();
+    bool bind(const int port);
+    bool listen() const;
+    bool accept(Socket&) const;
 
-  // Client initialization
-  bool connect ( const std::string host, const int port );
+    // Client initialization
+    bool connect(const std::string host, const int port);
 
-  // Data Transimission
-  bool send ( const std::string ) const;
-  int recv ( std::string& ) const;
-  bool has_data() const;
+    // Data Transimission
+    bool send(const std::string) const;
+    int recv(std::string&) const;
+    bool has_data() const;
 
+    void set_non_blocking(const bool);
+    sockaddr_in get_addr() const
+    {
+        return m_addr;
+    }
 
-  void set_non_blocking ( const bool );
-  sockaddr_in get_addr() const {return m_addr; }
+    bool is_valid() const
+    {
+        return m_sock != -1;
+    }
 
-  bool is_valid() const { return m_sock != -1; }
-
- protected:
-
-  int m_sock;
-  sockaddr_in m_addr;
-
-
+   protected:
+    int m_sock;
+    sockaddr_in m_addr;
 };
-
 
 #endif
