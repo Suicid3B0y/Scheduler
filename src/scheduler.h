fed0ba2686250c06a2468757c894f531c0df543c
Axel Martin <axel.martin@eisti.fr>
2016-05-02 16:20:20 +0000

    featureDoc: add the scheduler documentation.

--- src/scheduler.h
+++ src/scheduler.h
@@ -4,29 +4,57 @@
 #include "controller.h"
 #include <chrono>
 #include <thread>
+
+/**
+ * \class Scheduler
+ * \brief Class entirely managing the scheduler.
+ */
 class Scheduler {
 
-private:
-    unsigned timeSlice;
+    private:
+        unsigned timeSlice; /*!< Time at which the scheduler should update its jobs */
+        bool alive;  /*!< Check if the scheduler is alive. */
+        bool is_started;  /*!< Is the thread started */
 
-    bool alive;
-    bool is_started;
+        /**
+         * \brief Starts the scheduler.
+         */
+        int run();
 
-    int run();
+    public:
+        std::thread listening_thread;  /*!< Listening thread */
+        Controller controller;  /*!< Controller used for managing the jobs. */
 
-public:
-    std::thread listening_thread;
-    Controller controller;
-    Scheduler();
+        /**
+         * \brief Constructor.
+         */
+        Scheduler();
 
-    Scheduler(const Scheduler &other);
+        /**
+         * \brief Copy constructor.
+         */
+        Scheduler(const Scheduler &other);
 
-    Scheduler(unsigned core_number, unsigned timeout, unsigned timeSlice);
+        /**
+         * \brief Full constructor.
+         */
+        Scheduler(unsigned core_number, unsigned timeout, unsigned timeSlice);
 
-    Scheduler &operator=(const Scheduler &other);
-    bool isAlive();
+        /**
+         * \brief Assignment operator
+         */
+        Scheduler &operator=(const Scheduler &other);
 
-    void start();
+        /**
+         * \brief Check if the scheduler is currently working.
+         */
+        bool isAlive();
+
+        /**
+         * \brief Starts the scheduler.
+         */
+        void start();
+
 };
 
 #endif
