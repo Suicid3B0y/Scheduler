dad515350a1919a2cdc997c12dfdb0f14443b34e
Axel Martin <axel.martin@eisti.fr>
2016-05-02 13:33:21 +0000

    featureDoc: adding documentation of controller.h

--- src/controller.h
+++ src/controller.h
@@ -10,24 +10,55 @@
 
 using namespace std;
 
+/**
+ * \class Controller
+ * \brief Scheduler controller, managing which job will be start / stop depending of their priority.
+ *
+ */
 class Controller {
 private:
-    JobQueue jobQueue;
-    vector<job_ptr> currentJobs;
-    unsigned coreNumber;
+    JobQueue jobQueue; /*!< Priority queue containing all jobs */
+    vector<job_ptr> currentJobs; /*!< Vector containing all running jobs */
+    unsigned coreNumber; /*!< Number of cores the scheduler should use */
     unsigned timeout;
 
 public:
+    /**
+     * \brief Constructor
+     *
+     * Instantiate and empty controller
+     */
     Controller();
 
+    /**
+     * \brief Copy constructor
+     */
     Controller(const Controller &other);
 
+    /**
+     * \brief Constructor
+     *
+     * Create a controller based on a job queue, the numbers of cores allocated to the scheduler and the job timeout.
+     */
     Controller(JobQueue jobQueue, unsigned coreNumber,  unsigned timeout);
 
+    /**
+     * \brief Assignment operator
+     */
     Controller &operator=(const Controller &other);
 
+    /**
+     * \brief Update the running jobs
+     *
+     * Stops the jobs having a lower priority and starts the higher priority jobs
+     */
     vector<job_ptr> updateRunningJobs();
 
+    /**
+     * \brief Update the jobs queue
+     *
+     * Update the job queue by adding new jobs to the current jobQueue. An updateRunningJobs should be call after.
+     */
     void updateJobQueue(vector<Job>& newJobs);
 };
 
